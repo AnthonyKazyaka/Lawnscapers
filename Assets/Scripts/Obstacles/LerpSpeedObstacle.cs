a5e30771c561f7a9dd86a539d748fc26bbb96b0e
Anthony Kazyaka <ask.anthony@hotmail.com>
2016-08-20 23:44:53 -0400

    Fixed the moving obstacle using lerp speed.

--- Assets/Scripts/Obstacles/LerpSpeedObstacle.cs
+++ Assets/Scripts/Obstacles/LerpSpeedObstacle.cs
@@ -8,14 +8,20 @@ public class LerpSpeedObstacle : MovingObstacle
     private float _lerpCoefficient = 2.0f;
     public float LerpCoefficient { get { return _lerpCoefficient; } set { _lerpCoefficient = value; } }
 
+    [SerializeField]
+    private float _endPointSnapDistance = 0.05f;
+    public float EndPointSnapDistance { get { return _endPointSnapDistance; } set { _endPointSnapDistance = value; } }
+
     public override void Move()
     {
-        Vector3 newPosition; = (_direction * (EndTransform.position - StartTransform.position).normalized) * Time.deltaTime + gameObject.transform.position;
+        Vector3 newPosition = Vector3.zero;
         //Fix this. Don't need to do this calculation because we're going to Lerp anyways
 
         if (_direction == 1)
         {
-            if ((newPosition - StartTransform.position).magnitude > (EndTransform.position - StartTransform.position).magnitude)
+            newPosition = Vector3.Lerp(gameObject.transform.position, EndTransform.position, Time.deltaTime * LerpCoefficient);
+
+            if(Vector3.Distance(newPosition, EndTransform.position) <= EndPointSnapDistance)
             {
                 newPosition = EndTransform.position;
                 _direction *= -1;
@@ -24,7 +30,9 @@ public class LerpSpeedObstacle : MovingObstacle
         }
         else if (_direction == -1)
         {
-            if ((newPosition - EndTransform.position).magnitude > (EndTransform.position - StartTransform.position).magnitude)
+            newPosition = Vector3.Lerp(gameObject.transform.position, StartTransform.position, Time.deltaTime * LerpCoefficient);
+
+            if (Vector3.Distance(newPosition, StartTransform.position) <= EndPointSnapDistance)
             {
                 newPosition = StartTransform.position;
                 _direction *= -1;
@@ -34,7 +42,7 @@ public class LerpSpeedObstacle : MovingObstacle
 
         if (!IsCurrentlyAtEndPoint)
         {
-            gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, newPosition, Time.deltaTime * LerpCoefficient);
+            gameObject.transform.position = newPosition;
         }
     }
 }
